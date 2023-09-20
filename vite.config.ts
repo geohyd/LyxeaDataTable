7f3561c146e18ebf13e0fc99221c385706d5547b
Hugues Hertout <hugues.hertout@anteagroup.fr>
2023-09-20 15:52:11 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue"],
+      external: ["vue", "datatables.net-dt", "jquery"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
