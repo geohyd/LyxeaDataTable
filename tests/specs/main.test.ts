54450f49a4373378a5dfa8d4bc34761bf94a0f14
Hugues Hertout <hugues.hertout@anteagroup.fr>
2023-09-20 16:39:28 +0200

    update tests

--- tests/specs/main.test.ts
+++ tests/specs/main.test.ts
@@ -3,16 +3,16 @@
  */
 
 /** @ts-ignore */
-var $ = require( 'jquery' );
-var DataTable = require( 'datatables.net' )(window, $);
+var $ = require("jquery");
+var DataTable = require("datatables.net")(window, $);
 
 describe("Datatable should be up", () => {
-    test("dt is not null", () => {
-        let table = new DataTable('#myTable');
-        expect(table).toBeTruthy()
-    })
-})
+  test("dt is not null", () => {
+    let table = new DataTable("#myTable");
+    expect(table).toBeTruthy();
+  });
+});
 
 test("init", () => {
-    expect(1).toBe(1);
-})
+  expect(1).toBe(1);
+});
