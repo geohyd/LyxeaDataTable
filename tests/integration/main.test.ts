d0c4baec362d7909b0d6ff1f2553afe91539990b
Hugues Hertout <hugues.hertout@anteagroup.fr>
2023-09-21 14:21:05 +0200

    modify structure of tests

--- tests/integration/main.test.ts
+++ tests/integration/main.test.ts
@@ -1,3 +1,17 @@
-test("init", () => {
-  expect(1).toBe(1);
+/**
+ * @jest-environment jsdom
+ */
+
+/** @ts-ignore */
+import $ from "jquery";
+/**
+ * import this for impletement DOM environnement && `@jest-environment jsdom for testing datatable`
+ */
+var DataTable = require("datatables.net")(window, $);
+
+describe("Datatable should be up", () => {
+  test("dt is not null", () => {
+    let table = new DataTable("#myTable");
+    expect(table).toBeTruthy();
+  });
 });
--- tests/specs/main.test.ts
+++ tests/specs/main.test.ts
@@ -1,18 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-
-/** @ts-ignore */
-var $ = require("jquery");
-var DataTable = require("datatables.net")(window, $);
-
-describe("Datatable should be up", () => {
-  test("dt is not null", () => {
-    let table = new DataTable("#myTable");
-    expect(table).toBeTruthy();
-  });
-});
-
 test("init", () => {
   expect(1).toBe(1);
 });
