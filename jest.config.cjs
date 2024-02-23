24f4341ff951150b5f6307fce27dc72f98121589
Hugues Hertout <hugues.hertout@anteagroup.fr>
2024-02-23 11:23:20 +0100

    chore: adding dev environnement

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@dao(.*)$': '<rootDir>/lib/dao$1',
+    '^@dto(.*)$': '<rootDir>/lib/dto$1',
+    '^@dom(.*)$': '<rootDir>/lib/dom$1',
+    '^@core(.*)$': '<rootDir>/lib/core$1',
+    '^@plugins(.*)$': '<rootDir>/lib/plugins$1',
+  },
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    html: '<html lang="zh-cmn-Hant"><body></body></html>',
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,58 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@core': resolve(__dirname, 'lib/core'),
+      '@dao': resolve(__dirname, 'lib/dao'),
+      '@dto': resolve(__dirname, 'lib/dto'),
+      '@dom': resolve(__dirname, 'lib/dom'),
+      '@plugins': resolve(__dirname, 'lib/plugins'),
+    },
+  },
   build: {
     minify: true,
     cssMinify: true,
+    cssCodeSplit: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
-      name: "lx_dt",
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      name: 'lx_dt',
       // the proper extensions will be added
-      fileName: "lx_dt",
+      fileName: 'lx_dt',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: [
-        "vue",
-        "datatables.net-dt",
-        "jquery",
-        "husky",
-        "@types/jest",
-        "@types/jquery",
-        "@types/node",
-        "husky",
-        "jest",
-        "jest-environment-jsdom",
-        "lint-staged",
-        "prettier",
-        "ts-jest",
-        "typescript",
-        "vite",
-        "vite-plugin-dts",
+        'vue',
+        'jquery',
+        'husky',
+        '@types/jest',
+        '@types/jquery',
+        '@types/node',
+        'husky',
+        'jest',
+        'jest-environment-jsdom',
+        'lint-staged',
+        'prettier',
+        'ts-jest',
+        'typescript',
+        'vite',
+        'vite-plugin-dts',
       ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: "Vue",
+          vue: 'Vue',
         },
       },
     },
-    target: "esnext",
+    target: 'esnext',
   },
   plugins: [dts()],
 });
